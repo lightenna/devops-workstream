74376cbfa2d43913ee9e7f89a9030d0aefc54d4f
cleverlight <cleverlight@users.noreply.github.com>
2019-09-10 18:37:15 +0100

    hacked together new tutorial for pulling secrets into Puppet from eyaml

---
+++ puppet/tutorial/08-secret-templating-with-eyaml/manifests/site.pp
@@ -0,0 +1,11 @@
+#
+# site.pp
+# manifest for general configuration-managed hosts
+#
+# Command to execute:
+#     puppet apply -dvt ./manifests/site.pp --modulepath=./modules/ --hiera_config=./hiera.yaml
+#
+
+node default {
+  class { 'appserver' : }
+}
