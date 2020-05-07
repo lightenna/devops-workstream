d07f29886f90f71caa2ab1f9111960c2db2820f1
cleverlight <cleverlight@users.noreply.github.com>
2020-05-07 09:54:40 +0100

    added bootfacts for installing external facts

---
+++ vagrant/shared/scripts/bootfacts.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+## Install custom (external) facts onto the host
+##   $1 [role]
+##   $2 [environ]
+##   $3 [cluster]
+## Write into standard directory
+std_path=/etc/puppetlabs/facter/facts.d
+fact_file=$std_path/vg-ext-facts.yaml
+mkdir -p $std_path
+echo "# External facts file, created by bootfacts.sh" > $fact_file
+echo "---" >> $fact_file
+if [ $# -ge 1 ]; then
+    echo "role: \"$1\"" >> $fact_file
+fi
+if [ $# -ge 2 ]; then
+    echo "environ: \"$2\"" >> $fact_file
+fi
+if [ $# -ge 3 ]; then
+    echo "cluster: \"$3\"" >> $fact_file
+fi
