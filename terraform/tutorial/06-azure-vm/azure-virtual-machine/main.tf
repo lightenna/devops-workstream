3ab90e97a9903a7183f79bfce938625fe8c486b7
cleverlight <cleverlight@users.noreply.github.com>
2020-05-28 18:28:25 +0100

    removed deprecated network_security_group_id from subnet, now has its own attachment

--- terraform/tutorial/06-azure-vm/azure-virtual-machine/main.tf
+++ terraform/tutorial/06-azure-vm/azure-virtual-machine/main.tf
@@ -36,7 +36,6 @@ resource "azurerm_network_interface" "stnic" {
   name = "stnic-${local.hostbase}"
   location = "${var.resource_group_location}"
   resource_group_name = "${var.resource_group_name}"
-  network_security_group_id = "${var.nsg_id}"
 
   ip_configuration {
     name = "ipcfg-stnic-${local.hostbase}"
@@ -49,6 +48,12 @@ resource "azurerm_network_interface" "stnic" {
     name = "stnic-${local.hostbase}"
     environment = "${terraform.workspace}"
   }
+}
+
+# associate subnet with NSG
+resource "azurerm_subnet_network_security_group_association" "nsgsubnet" {
+  subnet_id                 = var.subnet_id
+  network_security_group_id = var.nsg_id
 }
 
 resource "azurerm_virtual_machine" "host" {
